05c2acf9f5c4c9ca6517b7bb6e3a1635227c6aa0
Subham mallick <125164275+Subham-Web98@users.noreply.github.com>
2024-05-01 00:22:35 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("input_box");
+const buttons = document.querySelectorAll("button");
+
+let string = ""; //* you are not attempting to reassign a value to a constant variable
+const arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
